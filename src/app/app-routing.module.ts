5440fe32399b855bcaf128a8c005820b0f3d51fd
Julia Georgiew <java2011@me.com>
2023-10-17 21:12:19 +0200

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { MainComponent } from './components/main/main.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { BoardComponent } from './components/board/board.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'home', component: HomeComponent,
+    
+    children: [
+      { path: '', component: MainComponent },
+      { path: 'summary', component: SummaryComponent },
+      { path: 'board', component: BoardComponent },
+      { path: 'contacts', component: ContactsComponent }
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { BoardComponent } from './components/board/board.component';
+import { AddtaskComponent } from './components/addtask/addtask.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    MainComponent,
+    SummaryComponent,
+    BoardComponent,
+    AddtaskComponent,
+    ContactsComponent
+   
   ],
   imports: [
     BrowserModule,
