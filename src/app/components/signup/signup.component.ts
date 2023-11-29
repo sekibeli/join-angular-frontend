daac355f6e8f32675baa5b1d06c5bbe0805e1436
Julia Georgiew <java2011@me.com>
2023-11-29 19:30:47 +0100

    authService signup

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -12,5 +14,17 @@ export class SignupComponent {
   password: string = '';
   errorMessage: string = '';
 
-  signUp(){}
+  constructor(private route:Router, private authService: AuthService){}
+  async signUp(){
+    try {
+      let resp: any = await this.authService.signup(this.vorname, this.nachname, this.email, this.password);
+      console.log(resp.token);
+      // localStorage.setItem('token', resp.token);
+      this.route.navigateByUrl('/login');
+      console.log(resp);
+    } catch (e) {
+      alert('Signup fehlgeschlagen - irgendwas ist schiefgegangen');
+      console.log('Fehler:', e);
+    }
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -22,4 +22,17 @@ export class AuthService {
     
     return lastValueFrom(this.http.post(url,body));
   }
+
+
+  signup(vorname: string, nachname: string, email: string, password: string):Promise<any>{
+    const url = environment.baseUrl + '/signup/';
+    const body = {
+      "first_name": vorname,
+      "last_name": nachname,
+      "username": email,
+      "password": password
+    }
+    
+    return lastValueFrom(this.http.post(url,body));
+  }
 }
