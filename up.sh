3a13dc7aa417b83e20d38fdd6311a530e7aa177c
Julia Georgiew <java2011@me.com>
2023-12-19 16:23:54 +0100

    change up.sh

--- up.sh
+++ up.sh
@@ -2,4 +2,4 @@ git add .
 git commit -m "$*"
 git push
 ng build
-git ftp push --syncroot dist/join-angular-frontend --remote-root angular-projects/join-angular-frontend
+git ftp push --syncroot dist/join-angular-frontend --remote-root angular-projects/myjoin
